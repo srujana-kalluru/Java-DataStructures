bbf57f5101af82c5aa1b31eff6d97eba39ebff86
creationk <creationk@users.noreply.github.com>
2022-06-23 15:30:13 +0530

    Initial commit

--- src/main/java/com/CustomLinkedList.java
+++ src/main/java/com/CustomLinkedList.java
@@ -4,15 +4,38 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CustomLinkedList {
+
+    class Node {
+        int data;
+        Node next;
+    }
+
     Node head;
     int size;
 
-    boolean addElementInTheBeginning(int val) {
+    public boolean contains(int key) {
+        if (null == head) {
+            return false;
+        }
+        Node temp = head;
+
+        while (null != temp) {
+            if (temp.data == key) {
+                return true;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return false;
+
+    }
+
+    public void addElementInTheBeginning(int val) {
         if (null == head) {
             head = new Node();
             head.data = val;
             size++;
-            return true;
+            return;
         }
 
         Node temp = new Node();
@@ -20,17 +43,16 @@ public class CustomLinkedList {
         temp.next = head;
         head = temp;
         size++;
-        return true;
     }
 
-    boolean addElementInTheEnd(int val) {
+    public void addElementInTheEnd(int val) {
         Node last = new Node();
         last.data = val;
 
         if (null == head) {
             head = last;
             size++;
-            return true;
+            return;
         }
 
         Node temp = head;
@@ -40,40 +62,112 @@ public class CustomLinkedList {
         }
         temp.next = last;
         size++;
-        return true;
     }
 
-    boolean addElementInTheGivenPosition(int val, int pos) {
-        int count = 0;
-        if (pos == 0 || null == head) {
-            addElementInTheBeginning(val);
-            return true;
-        }
-        if (pos == size) {
-            addElementInTheEnd(val);
-            return true;
-        }
 
+    public boolean addElementInTheGivenPosition(int val, int pos) {
         if (pos < 0 || pos > size) {
             return false;
         }
 
-        Node temp = head;
-        Node nodeToInsert = new Node();
-        nodeToInsert.data = val;
+        if (null == head || pos == 0) {
+            addElementInTheBeginning(val);
+        }
+        if (pos == size) {
+            addElementInTheEnd(val);
+        }
 
-        while (count < pos-1) {
+        int count = 0;
+
+        Node temp = head;
+
+        while (count < pos - 1) {
             temp = temp.next;
             count++;
         }
 
+        Node nodeToInsert = new Node();
+        nodeToInsert.data = val;
         nodeToInsert.next = temp.next;
         temp.next = nodeToInsert;
 
         return true;
+
     }
 
-    void printAllValuesInOrder() {
+    public boolean removeAnElementAtTheBeginning() {
+        if (null == head) {
+            return false;
+        }
+        head = head.next;
+        size--;
+        return true;
+
+    }
+
+    public boolean removeAnElementAtTheEnd() {
+        if (null == head) {
+            return false;
+        }
+
+        Node temp = head;
+
+        while (null != (temp.next).next) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        size--;
+        return true;
+
+    }
+
+    public boolean removeElementAtAGivenPosition(int pos) {
+        if (null == head || pos >= size || pos < 0) {
+            return false;
+        }
+        if (pos == 0) {
+            removeAnElementAtTheBeginning();
+        }
+        if (pos == size - 1) {
+            removeAnElementAtTheEnd();
+        }
+
+        Node temp = head;
+        int count = 0;
+        while (count < pos - 1) {
+            temp = temp.next;
+            count++;
+        }
+
+        temp.next = temp.next.next;
+        size--;
+        return true;
+    }
+
+
+    public boolean removeAKeyFromTheList(int key) {
+        if (null == head) {
+            return false;
+        }
+        if (head.data == key) {
+            removeAnElementAtTheBeginning();
+        }
+        Node temp = head;
+
+        while (null != (temp.next).next) {
+            if ((temp.next).data == key) {
+                temp.next = temp.next.next;
+                size--;
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+
+    public void printAllValuesInOrder() {
         if (null == head) {
             log.info("Empty list!");
             return;
@@ -86,7 +180,4 @@ public class CustomLinkedList {
     }
 }
 
-class Node {
-    int data;
-    Node next;
-}
+
--- src/test/java/com/TestCustomLinkedList.java
+++ src/test/java/com/TestCustomLinkedList.java
@@ -1,55 +1,142 @@
 package com;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class TestCustomLinkedList {
-    @Test
-    void testAddElementInTheBeginning (){
-        CustomLinkedList ll = new CustomLinkedList();
-        log.info(String.valueOf(ll.addElementInTheBeginning(10)));
-        log.info(String.valueOf(ll.addElementInTheBeginning(20)));
-        log.info(String.valueOf(ll.addElementInTheBeginning(30)));
-        log.info(String.valueOf(ll.addElementInTheBeginning(40)));
 
-        ll.printAllValuesInOrder();
+    @Test
+    void testContains() {
+        CustomLinkedList ll = new CustomLinkedList();
+
+        ll.addElementInTheBeginning(10);
+        ll.addElementInTheBeginning(20);
+        ll.addElementInTheBeginning(30);
+        ll.addElementInTheBeginning(40);
+
+        log.info(String.valueOf(ll.contains(40)));
     }
 
     @Test
-    void testAddElementInTheEnd (){
-        CustomLinkedList ll = new CustomLinkedList();
-        log.info(String.valueOf(ll.addElementInTheEnd(50)));
-        log.info(String.valueOf(ll.addElementInTheEnd(60)));
-        log.info(String.valueOf(ll.addElementInTheEnd(70)));
-        log.info(String.valueOf(ll.addElementInTheEnd(80)));
-
-        ll.printAllValuesInOrder();
-    }
-
-    @Test
-    void testAddElementAtAGivenPosition (){
+    void testAddElementInTheBeginning() {
         CustomLinkedList ll = new CustomLinkedList();
         ll.addElementInTheBeginning(10);
         ll.addElementInTheBeginning(20);
         ll.addElementInTheBeginning(30);
         ll.addElementInTheBeginning(40);
 
-        log.info(String.valueOf(ll.addElementInTheGivenPosition(100,2)));
+        ll.printAllValuesInOrder();
+    }
+
+    @Test
+    void testAddElementInTheEnd() {
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(50);
+        ll.addElementInTheEnd(60);
+        ll.addElementInTheEnd(70);
+        ll.addElementInTheEnd(80);
 
         ll.printAllValuesInOrder();
     }
 
     @Test
-    void testAddElementAtAGivenPositionFail (){
+    void testAddElementAtAGivenPosition() {
         CustomLinkedList ll = new CustomLinkedList();
-        ll.addElementInTheBeginning(10);
-        ll.addElementInTheBeginning(20);
-        ll.addElementInTheBeginning(30);
-        ll.addElementInTheBeginning(40);
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
 
-        log.info(String.valueOf(ll.addElementInTheGivenPosition(100,4)));
+        log.info(String.valueOf(ll.addElementInTheGivenPosition(100, 2)));
+        log.info(String.valueOf(ll.addElementInTheGivenPosition(0, 1)));
 
         ll.printAllValuesInOrder();
     }
+
+    @Test
+    void removeElementInTheBeginning() {
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
+
+        ll.removeAnElementAtTheBeginning();
+
+        ll.printAllValuesInOrder();
+        Assertions.assertFalse(ll.contains(10));
+    }
+
+    @Test
+    void removeElementInTheEnd() {
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
+
+        ll.removeAnElementAtTheEnd();
+        Assertions.assertFalse(ll.contains(40));
+
+        ll.printAllValuesInOrder();
+    }
+
+    @Test
+    void testRemoveElementAtAGivenPosition() {
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
+
+        log.info(String.valueOf(ll.removeElementAtAGivenPosition(2)));
+        Assertions.assertFalse(ll.contains(30));
+
+        ll.printAllValuesInOrder();
+    }
+
+    @Test
+    void testRemoveElementAtAGivenPositionFail() {
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
+
+        log.info(String.valueOf(ll.removeElementAtAGivenPosition(4)));
+
+        ll.printAllValuesInOrder();
+    }
+
+    @Test
+    void testAddElementAtAGivenPositionFail() {
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
+
+        log.info(String.valueOf(ll.addElementInTheGivenPosition(100, 4)));
+        Assertions.assertTrue(ll.contains(100));
+
+        ll.printAllValuesInOrder();
+    }
+
+    @Test
+    void testRemoveAKeyFromTheList() {
+        int keyToRemove = 30;
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.addElementInTheEnd(10);
+        ll.addElementInTheEnd(20);
+        ll.addElementInTheEnd(30);
+        ll.addElementInTheEnd(40);
+        ll.removeAKeyFromTheList(keyToRemove);
+        Assertions.assertFalse(ll.contains(30));
+
+        ll.printAllValuesInOrder();
+
+    }
+
 }
